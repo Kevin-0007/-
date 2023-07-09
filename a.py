d27691702b448baf7076c2a984d3a464762bd7ec
Kevin-0007 <68748345+Kevin-0007@users.noreply.github.com>
2023-07-09 20:26:52 +0800

    Update a.py

--- a.py
+++ a.py
@@ -1,7 +1,7 @@
 import glob,os
-file_path = r"C:\Users\admin\Desktop\live"
+file_path = "/root/work/session"
 xlsx_files = glob.glob(file_path + "/*.session")
-ip_list=['2404:8c80:0:1009:5c:e8f4:f8c0:2d3b','2404:8c80:0:1009:ce:1165:1d01:cb9c']
+ip_list=['2404:8c80:0:1007:1f:a279:4446:b6f3','2404:8c80:0:1007:dd:1fa5:7de0:9d01']
 index=1
 for i in xlsx_files:
     all=len(xlsx_files)
@@ -10,8 +10,7 @@ for i in xlsx_files:
         ip=ip_list[1]
     ii=i.replace(file_path,'/root/work')
     py_file=ii.replace('.session','.py')
-    msg='from dj import work'+'\n'+'phone="'+i+'"\n'+'work(phone,'+ip+')'
-    print(msg)
+    msg='from dj import work'+'\n'+'phone="'+i+'"\n'+'work(phone,"'+ip+'")'
     index+=1
-    #open(py_file,'a+').write(msg)
-    #print(i)
+    open(py_file,'a+').write(msg)
+    print(i)

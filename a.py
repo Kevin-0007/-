4e21477185f6450818aec31b23b287252423282b
Kevin-0007 <68748345+Kevin-0007@users.noreply.github.com>
2023-07-09 19:10:55 +0800

    Update a.py

--- a.py
+++ a.py
@@ -1,12 +1,16 @@
+from telethon import TelegramClient
+
+api_id=2496
+api_hash='8da85b0d5bfe62527e5b244c209159c3'
 with TelegramClient(
     '6283114514550', 
     api_id, 
     api_hash,
     use_ipv6=True,
     local_addr='2404:8c80:0:1009:ce:1165:1d01:cb9c',
-    device_model='iphone 16 Pro Max',
-    system_version='IOS16.6.6',
-    lang_code='zh'
-    
+    device_model='UFO',
+    system_version='UFO8',
+    lang_code='zh',
+    app_version='Telelgram Desktop 4.6.5 x64'
     ) as client:
     client.start()

feeb567c9619b5f74b8b3ba2b291418cd9f2ec24
Kevin-0007 <68748345+Kevin-0007@users.noreply.github.com>
2023-07-09 20:20:34 +0800

    Update a.py

--- a.py
+++ a.py
@@ -1,15 +1,17 @@
-from telethon import TelegramClient
-
-api_id =2040
-api_hash = 'b18441a1ff607e10a989891a5462e627'
-with TelegramClient(
-    '6282323894484', 
-    api_id, 
-    api_hash,
-    use_ipv6=True,
-    local_addr='2404:8c80:0:1009:ce:1165:1d01:cb9c',
-    device_model="OPPO a37",
-    system_version='Android 8.8.8',
-    app_version='Telegram Linux'
-    ) as client:
-    client.start()
+import glob,os
+file_path = r"C:\Users\admin\Desktop\live"
+xlsx_files = glob.glob(file_path + "/*.session")
+ip_list=['2404:8c80:0:1009:5c:e8f4:f8c0:2d3b','2404:8c80:0:1009:ce:1165:1d01:cb9c']
+index=1
+for i in xlsx_files:
+    all=len(xlsx_files)
+    ip=ip_list[0]
+    if index>all/2:
+        ip=ip_list[1]
+    ii=i.replace(file_path,'/root/work')
+    py_file=ii.replace('.session','.py')
+    msg='from dj import work'+'\n'+'phone="'+i+'"\n'+'work(phone,'+ip+')'
+    print(msg)
+    index+=1
+    #open(py_file,'a+').write(msg)
+    #print(i)

9299fbd9723883e6f85e4cf787933209d7c759ab
Kevin-0007 <68748345+Kevin-0007@users.noreply.github.com>
2023-07-09 19:05:12 +0800

    Create a.py

---
+++ a.py
@@ -0,0 +1,12 @@
+with TelegramClient(
+    '6283114514550', 
+    api_id, 
+    api_hash,
+    use_ipv6=True,
+    local_addr='2404:8c80:0:1009:ce:1165:1d01:cb9c',
+    device_model='iphone 16 Pro Max',
+    system_version='IOS16.6.6',
+    lang_code='zh'
+    
+    ) as client:
+    client.start()

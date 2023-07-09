a495d3cca5444688905ecf4eb246e516ee2263ef
Kevin-0007 <68748345+Kevin-0007@users.noreply.github.com>
2023-07-09 20:01:07 +0800

    Update a.py

--- a.py
+++ a.py
@@ -1,13 +1,15 @@
 from telethon import TelegramClient
 
-api_id=17349 
-api_hash='344583e45741c457fe1862106095a5eb'
+api_id =2040
+api_hash = 'b18441a1ff607e10a989891a5462e627'
 with TelegramClient(
-    '6283114514561', 
+    '6282323894484', 
     api_id, 
     api_hash,
-    device_model="MEIZU 5G",
-    system_version=' Android 8.8.8',
-    app_version='Telegram Android'
+    use_ipv6=True,
+    local_addr='2404:8c80:0:1009:ce:1165:1d01:cb9c',
+    device_model="OPPO a37",
+    system_version='Android 8.8.8',
+    app_version='Telegram Linux'
     ) as client:
     client.start()
